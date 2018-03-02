67083678b3c7c240c25d5b6486ff3461d807c23f
Aurimas Šimkus <saurimas@hotmail.com>
2018-03-02 22:17:47 +0200

    CORS enabled

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, { origins: '*:*'});
 var port = process.env.PORT || 3055;
 
 app.get('/', function(req, res){
