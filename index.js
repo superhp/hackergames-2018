c5f53c89ab078336b3510bf5d128225ad4a8cbd9
Artūras Bončkus <artis.b45@live.com>
2018-03-03 00:49:40 +0200

    Added user list, login and disconect operations

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, { origins: '*:*'});
 var port = process.env.PORT || 1337;
+var connectedUsers = []
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,6 +15,21 @@ io.on('connection', function(socket){
 
   socket.on('private', function(id, msg){ 
     socket.broadcast.to(id).emit('private', msg); 
+  });
+
+  socket.on('user list', function(){
+    var data = connectedUsers.map(user => Object({name : user.profile.name, socketId : user.socketId}))
+    io.emit('user list', data)
+  });
+
+  socket.on('login', function(profile){
+    connectedUsers.push({profile : profile, socketId : socket.id})
+    var data = connectedUsers.map(user => Object({name : user.profile.name, socketId : user.socketId}))
+    io.emit('user list', data)
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
 });
 
