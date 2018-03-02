12bc35a6284cfe131dae07a199cae52645499252
Aurimas Šimkus <saurimas@hotmail.com>
2018-03-03 00:05:24 +0200

    Improved private conversation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http, { origins: '*:*'});
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 3055;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,8 +12,9 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 
-  socket.on('private', function(id, msg){ 
-    socket.broadcast.to(id).emit('private', msg); 
+  socket.on('private', function(receiverId, message){ 
+    console.log(receiverId + " " + message);
+    socket.broadcast.to(receiverId).emit('private', {senderId: socket.id, message: message}); 
   });
 });
 
