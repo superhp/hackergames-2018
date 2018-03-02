907626894ee7113d24e60cf12a5af0244f82e354
Aurimas Šimkus <saurimas@hotmail.com>
2018-03-02 22:08:09 +0200

    Socket.io added

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3055;
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on *:3000');
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
