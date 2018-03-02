50335a390c5b1be000689e7d41109f46189315be
Aurimas Šimkus <saurimas@hotmail.com>
2018-03-03 00:42:26 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http, { origins: 'https://learnfromme.azurewebsites.net'});
+var io = require('socket.io')(http);
 var port = process.env.PORT || 1337;
 
 app.get('/', function(req, res){
